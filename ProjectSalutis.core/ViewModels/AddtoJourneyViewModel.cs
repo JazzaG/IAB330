37d593e9df6cf6f73bc96709c472f55225c1cd77
Samuel Dekker <samuel.dekker@student.qut.edu.au>
2016-09-04 22:30:27 +1000

    Manually merged Journey Branch because it was easier

---
+++ ProjectSalutis.core/ViewModels/AddtoJourneyViewModel.cs
@@ -0,0 +1,30 @@
+﻿using MvvmCross.Core.ViewModels;
+using System.Windows.Input;
+
+namespace ProjectSalutis.core.ViewModels
+{
+    public class AddtoJourneyViewModel
+        : MvxViewModel
+    {
+
+        private double sliderValue;
+        public double SliderValue
+        {
+            get { return sliderValue; }
+            set
+            {
+                SetProperty(ref sliderValue, value);
+            }
+        }
+
+        public ICommand ButtonCommand { get; private set; }
+        public AddtoJourneyViewModel()
+        {
+            ButtonCommand = new MvxCommand(() =>
+            {
+                //do nothing
+            });
+        }
+
+    }
+}
---
+++ ProjectSalutis.droid/Views/AddtoJourneyView.cs
@@ -0,0 +1,31 @@
+using Android.App;
+using Android.OS;
+using Android.Widget;
+using MvvmCross.Droid.Views;
+using System;
+
+// Author: Samuel Dekker
+// Will need to be switched across to work with MVVMX
+
+namespace ProjectSalutis.droid.Views
+{
+    [Activity(Label = "Add to Journey")]
+    public class AddtoJourneyView : MvxActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.AddtoJourneyView);
+
+            Spinner spinner = FindViewById<Spinner>(Resource.Id.journey_spinner);
+            SeekBar seekbar = FindViewById<SeekBar>(Resource.Id.feeling_seekbar);
+            Button add = FindViewById<Button>(Resource.Id.add_button);
+
+            add.Click += delegate {
+                string toast = string.Format("{0}/10 on {1} journey", seekbar.Progress, spinner.SelectedItem);
+                Toast.MakeText(this, toast, ToastLength.Long).Show();
+            };
+        }
+    }
+}
