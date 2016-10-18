19908367109c0592fa430f36988ca324df4ad359
Samuel Dekker <samuel.dekker@student.qut.edu.au>
2016-10-18 13:53:53 +1000

    Fixed invalid axml

--- ProjectSalutis.droid/Resources/Resource.designer.cs
+++ ProjectSalutis.droid/Resources/Resource.designer.cs
@@ -218,22 +218,28 @@ namespace ProjectSalutis.droid
 			public const int AddtoJourneyView = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int CreateGoal = 2130903041;
+			public const int CreateGoalView = 2130903041;
 			
 			// aapt resource value: 0x7f030002
 			public const int FirstView = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int JourneyEntryLayout = 2130903043;
+			public const int GoalListView = 2130903043;
 			
 			// aapt resource value: 0x7f030004
-			public const int JourneyView = 2130903044;
+			public const int JourneyEntryLayout = 2130903044;
 			
 			// aapt resource value: 0x7f030005
-			public const int NewContactView = 2130903045;
+			public const int JourneyView = 2130903045;
 			
 			// aapt resource value: 0x7f030006
-			public const int SplashScreen = 2130903046;
+			public const int ListItem_Goal = 2130903046;
+			
+			// aapt resource value: 0x7f030007
+			public const int NewContactView = 2130903047;
+			
+			// aapt resource value: 0x7f030008
+			public const int SplashScreen = 2130903048;
 			
 			static Layout()
 			{
