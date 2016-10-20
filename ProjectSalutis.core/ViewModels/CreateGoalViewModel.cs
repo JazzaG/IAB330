c7919729a7b1bcb38e3386174e7dad83c01c0ed7
Jared Gharib <jazzag69@gmail.com>
2016-10-20 16:51:53 +1000

    Switch to MvxSpinner

--- ProjectSalutis.core/ViewModels/CreateGoalViewModel.cs
+++ ProjectSalutis.core/ViewModels/CreateGoalViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.Generic;
+using System.Windows.Input;
 using MvvmCross.Core.ViewModels;
 
 namespace ProjectSalutis.core.ViewModels
@@ -11,6 +12,22 @@ namespace ProjectSalutis.core.ViewModels
 		public string Quantity { get; set; }
 		public string Frequency { get; set; }
 		public string Duration { get; set; }
+
+		public List<string> GoalTypes
+		{
+			get
+			{
+				return new List<string>() 
+				{ 
+					"Bike Ride",
+					"Run / Walk / Jog",
+					"Swim",
+					"Pilates",
+					"Yoga",
+					"Exercise"
+				};
+			}
+		}
 
 		public ICommand AddButtonClick
 		{
