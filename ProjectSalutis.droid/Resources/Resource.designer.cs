e4ec924361e85bc0cfd0946527a51c1dd61f39b5
Jared Gharib <jazzag69@gmail.com>
2016-10-20 14:33:11 +1000

    Work on goals screen

--- ProjectSalutis.droid/Resources/Resource.designer.cs
+++ ProjectSalutis.droid/Resources/Resource.designer.cs
@@ -310,16 +310,13 @@ namespace ProjectSalutis.droid
 			public const int JourneyView = 2130903044;
 			
 			// aapt resource value: 0x7f030005
-			public const int ListItem_Goal = 2130903045;
+			public const int NavigationView = 2130903045;
 			
 			// aapt resource value: 0x7f030006
-			public const int NavigationView = 2130903046;
+			public const int NewContactView = 2130903046;
 			
 			// aapt resource value: 0x7f030007
-			public const int NewContactView = 2130903047;
-			
-			// aapt resource value: 0x7f030008
-			public const int SplashScreen = 2130903048;
+			public const int SplashScreen = 2130903047;
 			
 			static Layout()
 			{
