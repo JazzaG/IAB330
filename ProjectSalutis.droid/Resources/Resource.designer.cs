c7fdfd4fed855e7bc3dfca8d006dc2ba0087d09c
Jared Gharib <jazzag69@gmail.com>
2016-10-31 23:50:09 +1000

    Show notification when user creates goal

--- ProjectSalutis.droid/Resources/Resource.designer.cs
+++ ProjectSalutis.droid/Resources/Resource.designer.cs
@@ -191,13 +191,16 @@ namespace ProjectSalutis.droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int logo = 2130837504;
+			public const int ic_notification = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int scale = 2130837505;
+			public const int logo = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int splash = 2130837506;
+			public const int scale = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int splash = 2130837507;
 			
 			static Drawable()
 			{
--- ProjectSalutis.droid/Views/CreateGoalView.cs
+++ ProjectSalutis.droid/Views/CreateGoalView.cs
@@ -1,12 +1,14 @@
 ﻿// Author: Jared Gharib
 
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
 using Android.Util;
 using Android.Widget;
 using MvvmCross.Core.ViewModels;
 using MvvmCross.Droid.Views;
+using MvvmCross.Platform;
 using ProjectSalutis.core.ViewModels;
 
 namespace ProjectSalutis.droid.Views
@@ -48,7 +50,24 @@ namespace ProjectSalutis.droid.Views
 
 		private void OnAddButtonClick()
 		{
-			Toast.MakeText(this, "Add button clicked", ToastLength.Short).Show();
+			// Build PendingIntent to take user to goal list screen
+			// Thanks http://stackoverflow.com/a/18408355
+			var requestToGoalListScreen = MvxViewModelRequest<GoalListViewModel>.GetDefaultRequest();
+			var androidViewModelRequestTranslator = Mvx.Resolve<IMvxAndroidViewModelRequestTranslator>();
+			var intent = androidViewModelRequestTranslator.GetIntentFor(requestToGoalListScreen);
+			var pendingIntent = PendingIntent.GetActivity(this, 0, intent, 0);
+
+			// Build the notification
+			Notification.Builder builder = new Notification.Builder(this)
+				.SetAutoCancel(true)
+				.SetContentIntent(pendingIntent)
+				.SetSmallIcon(Resource.Drawable.ic_notification)
+				.SetContentTitle("Project Salutis")
+				.SetContentText("You have goals to work towards!");
+
+			// Show the notification
+			NotificationManager manager = (NotificationManager)GetSystemService(Context.NotificationService);
+			manager.Notify(1000, builder.Build());
 		}
 
 	}
