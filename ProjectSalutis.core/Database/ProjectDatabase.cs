5fc3ce0581370b468029e28f87fbab50d3d63a04
Samuel Dekker <samuelcdekker@gmail.com>
2016-11-02 13:58:29 +1000

    Fills database with sample Journey entries automatically on the first load

--- ProjectSalutis.core/Database/ProjectDatabase.cs
+++ ProjectSalutis.core/Database/ProjectDatabase.cs
@@ -20,6 +20,28 @@ namespace ProjectSalutis.core.Database
             database.CreateTable<JourneyEntry>();
 			database.CreateTable<Goal>();
             database.CreateTable<Contact>();
+
+            //method to fill entries manually for demo
+            if (database.Table<JourneyEntry>().Count() == 0) {
+                AddInitialJourneyEntries();
+            }
+        }
+
+        public async void AddInitialJourneyEntries()
+        {
+            int year = DateTime.Now.Year;
+            int month = DateTime.Now.Month;
+            int day = DateTime.Now.Day;
+
+            await InsertJourneyEntry(new Models.JourneyEntry(new DateTime(year, month, day, 09, 16, 17).Subtract(new TimeSpan(7, 0, 0, 0)), "Exercise", 4, "Started pilates"));
+            await InsertJourneyEntry(new Models.JourneyEntry(new DateTime(year, month, day, 15, 28, 42).Subtract(new TimeSpan(6, 0, 0, 0)), "Happiness", 4, "Saw a dog"));
+            await InsertJourneyEntry(new Models.JourneyEntry(new DateTime(year, month, day, 12, 25, 25).Subtract(new TimeSpan(5, 0, 0, 0)), "Nutrition", 3, "Vegetable bolognaise"));
+            await InsertJourneyEntry(new Models.JourneyEntry(new DateTime(year, month, day, 13, 30, 12).Subtract(new TimeSpan(4, 0, 0, 0)), "Exercise", 2, "Short walk"));
+            await InsertJourneyEntry(new Models.JourneyEntry(new DateTime(year, month, day, 20, 57, 23).Subtract(new TimeSpan(4, 0, 0, 0)), "Happiness", 3, "Movie with friends"));
+            await InsertJourneyEntry(new Models.JourneyEntry(new DateTime(year, month, day, 18, 43, 34).Subtract(new TimeSpan(3, 0, 0, 0)), "Nutrition", 4, "Chickpea and spinach pasta"));
+            await InsertJourneyEntry(new Models.JourneyEntry(new DateTime(year, month, day, 09, 06, 59).Subtract(new TimeSpan(2, 0, 0, 0)), "Exercise", 4, "Completed pilates goal"));
+            await InsertJourneyEntry(new Models.JourneyEntry(new DateTime(year, month, day, 18, 35, 51).Subtract(new TimeSpan(1, 0, 0, 0)), "Nutrition", 2, "Sushi"));
+            await InsertJourneyEntry(new Models.JourneyEntry(new DateTime(year, month, day, 18, 36, 23).Subtract(new TimeSpan(1, 0, 0, 0)), "Happiness", 3, "Sushi!"));
         }
 
         public async Task<IEnumerable<JourneyEntry>> GetJourneyEntries()
--- ProjectSalutis.core/Models/JourneyEntry.cs
+++ ProjectSalutis.core/Models/JourneyEntry.cs
@@ -33,6 +33,36 @@ namespace ProjectSalutis.core.Models
             Notes = notes;
         }
 
+        /*
+         * Method only used for adding entries manually for demo
+         */ 
+        public JourneyEntry(DateTime datetime, string category, int rating, string notes)
+        {
+            Timestamp = datetime;
+            Timestamp_string = Timestamp.ToString(); //required for the time to show as not UTC
+            Category = category;
+            Rating = rating;
+            switch (rating)
+            {
+                case 0:
+                    RatingDrawable = "very_dissatisfied";
+                    break;
+                case 1:
+                    RatingDrawable = "dissatisfied";
+                    break;
+                case 2:
+                    RatingDrawable = "neutral";
+                    break;
+                case 3:
+                    RatingDrawable = "satisfied";
+                    break;
+                case 4:
+                    RatingDrawable = "very_satisfied";
+                    break;
+            }
+            Notes = notes;
+        }
+
         public DateTime Timestamp { get; set; }
         public string Timestamp_string { get; set; }
         public string Category { get; set; }
